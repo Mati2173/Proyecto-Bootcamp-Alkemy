7d96ca84df0b1287b583f26cae91c350c5cf52e9
Mati <matigomez2173@gmail.com>
2023-05-07 21:49:12 -0300

    Vistas

--- compra/views.py
+++ compra/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Product, Supplier
+
 
 # Create your views here.
+def products_list(request):
+    products = Product.objects.all()
+    cxt = {'products': products}
+    return render(request, 'products.jinja', cxt)
+
+
+def suppliers_list(request):
+    suppliers = Supplier.objects.all()
+    cxt = {'suppliers': suppliers}
+    return render(request, 'suppliers.jinja', cxt)
+
+
+def create_product(request):
+    pass
+
+
+def create_supplier(request):
+    pass
